e98056c6ae5229f220da1f1a55892431d2daf82c
Piotr Grzybowski <grzybowski_p@wp.pl>
2023-04-18 23:42:35 +0200

    card number formater JS

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
-const cardNumberInput = document.querySelector("#inputCardNumber");
+const cardNumberInput = document.querySelector("#cardNumberInput");
 const cardNumberImg = document.querySelector("#card-number");
 const cardHolderImg = document.querySelector("#card-holder");
-const cardHolderInput = document.querySelector("#inputCardHolder");
+const cardHolderInput = document.querySelector("#cardHolderInput");
 
 const form = document.querySelector("form");
 
-cardHolderInput.addEventListener("input", updateValue)
+// card holder input handler
+cardHolderInput.addEventListener("input", (e) => {
+	cardHolderImg.textContent = e.target.value;
+});
+
+// card number input handler
+cardNumberInput.addEventListener("input", updateValue);
 function updateValue(e) {
-   cardHolderImg.textContent = (e.target.value);
+	let value = e.target.value;
+	cardNumberImg.textContent = numberFormat(value);
 }
 
+function numberFormat(value) {
+	const regex = /^(\d{0,4})(\d{0,4})(\d{0,4})(\d{0,4})$/g;
+	return value.replace(regex, (regex, $1, $2, $3, $4) =>
+		[$1, $2, $3, $4].filter((group) => !!group).join(" ")
+	);
+}
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-    mainFn();
 });
