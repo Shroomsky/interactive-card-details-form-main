32745966cd8efed08c57ab6a0eb5c2757492aa96
Piotr Grzybowski <grzybowski_p@wp.pl>
2023-04-19 09:51:29 +0200

    input handler regexp added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const cardNumberInput = document.querySelector("#cardNumberInput");
 const cardNumberImg = document.querySelector("#card-number");
 const cardHolderImg = document.querySelector("#card-holder");
 const cardHolderInput = document.querySelector("#cardHolderInput");
+const errorCardNumber = document.getElementById("errorCardNumber");
+const completeCard = document.querySelector(".complete");
 
 const form = document.querySelector("form");
 
@@ -11,10 +13,33 @@ cardHolderInput.addEventListener("input", (e) => {
 });
 
 // card number input handler
+
 cardNumberInput.addEventListener("input", updateValue);
 function updateValue(e) {
 	let value = e.target.value;
-	cardNumberImg.textContent = numberFormat(value);
+	if (!onlyNumbersCheck(value)) {
+		cardNumberImg.textContent = numberFormat(value);
+		cardNumberInput.classList.remove("error");
+		errorCardNumber.classList.add("hide-content");
+	} else {
+		console.log("only digits please");
+		cardNumberInput.classList.add("error");
+		errorCardNumber.classList.remove("hide-content");
+	}
+}
+
+// check functions
+
+function onlyNumbersCheck(value) {
+	const regex = /[a-z]/;
+	res = regex.test(value);
+	return res;
+}
+
+function isBlankCheck(value) {
+	const regex = /./;
+	res = !regex.test(value);
+	return res;
 }
 
 function numberFormat(value) {
@@ -24,6 +49,13 @@ function numberFormat(value) {
 	);
 }
 
+function submitForm() {
+	form.classList.toggle("hide-content");
+	completeCard.classList.toggle("hide-content");
+}
+
+//app start
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
+	submitForm();
 });
